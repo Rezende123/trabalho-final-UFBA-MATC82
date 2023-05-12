a66e146a059968cc1bf4c555dc633f1bb1cc4c13
Meta@31501 <felipe.rezende@meta.com.br>
2023-05-11 22:36:35 -0300

    Carrega o texto aleatório com as imagens aleatórioas

--- scripts/pagina1.js
+++ scripts/pagina1.js
@@ -1,6 +1,71 @@
 $(document).ready(() => {
-  $('#conteudo').on('DOMNodeInserted', '#Pag1', () => {
-    // É chamado quando o conteúdo é inserido
-    console.log('Pagina 1')
-  })
+  // É chamado quando o conteúdo é inserido
+  buscaFrases()
+    .then(montaTexto)
 })
+
+function buscaFrases() {
+  return new Promise((res, rej) => {
+    $.ajax({
+      url: 'https://type.fit/api/quotes',
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        res(data);
+      },
+      error: function (data) {
+        rej(data);
+      }
+    });
+  })
+}
+
+function buscaGif() {
+  return new Promise((res, rej) => {
+    $.ajax({
+      url: 'https://dog.ceo/api/breeds/image/random',
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        res(data);
+      },
+      error: function (data) {
+        rej(data);
+      }
+    });
+  })
+}
+
+function montaTexto(frases) {
+  const obtemNumeroAleatorio = () => {
+    const min = 0, max = frases.length - 1
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+  const inserirNovoCard = () => {
+    const indice = obtemNumeroAleatorio();
+    const frase = frases[indice];
+    const { text, author } = frase;
+
+    buscaGif()
+      .then(({message: gif}) => {
+        const card = $(`
+          <div class="card m-3 col-4">
+          <img class="card-img-top" src="${gif}" alt="Gif Aleatorio">
+            <div class="card-body">
+              <h5 class="card-title">${author}</h5>
+              <p class="card-text">${text}</p>
+            </div>
+          </div>
+        `);
+    
+        $('#conteudo').append(card);
+      })
+  }
+
+  const quantidadeCards = 10
+
+  for (let index = 0; index < quantidadeCards; index++)
+    inserirNovoCard();
+
+}
